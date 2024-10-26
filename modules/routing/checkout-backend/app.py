1020dc773b3eafae19ed2005379f80b99319a28a
py1u.dev <py1u.dev@gmail.com>
2024-10-25 22:13:09 -0700

    update: create get cart endpoint

--- modules/routing/checkout-backend/app.py
+++ modules/routing/checkout-backend/app.py
@@ -48,5 +48,64 @@ def get_product_by_id(product_id):
         return json.dumps({"success": False, "error": "Product not found!"})
     return json.dumps({"success": True, "data": product})
     
+@app.route("/cart/")
+def get_cart():
+    data = list(cart.values())
+    if cart_item_count == 0:
+        return json.dumps({"data": "cart is empty!"}),200
+    return json.dumps({"success": True, "data": data}),200
+
+@app.route("/cart/", methods=["POST"])
+def add_product_to_cart():
+    global cart_item_count
+    body = json.loads(request.data)
+    product_id = body.get("id")
+    product = products.get(product_id)
+    
+    if not product:
+        return json.dumps({"success": False, "error": "Product not found!"}), 404
+    
+    item = {
+        "id": product_id,
+        "name": product.get("name"),
+        "price": product.get("price"),
+        "quantity": product.get("quantity")
+    }
+    if not item:
+        return json.dumps({"success": False, "data": "item not found!"}), 404
+    cart[cart_item_count] = item
+    cart_item_count += 1 
+    print(cart_item_count)
+    return json.dumps({"success": True, "data": item}), 201
+        
+@app.route("/cart/", methods=["DELETE"])
+def delete_cart_product():
+    body = json.loads(request.data)
+    product_id = body.get("id")
+    product = products.get(product_id)
+    
+    if not product:
+        return json.dumps({"success": False, "error": "Product not found!"}), 404
+    
+    item = {
+        "id": product_id,
+        "name": product.get("name"),
+        "price": product.get("price"),
+        "quantity": product.get("quantity")
+    }
+    
+    if not item:
+        return json.dumps({"sucess": False, "data": "item not found!"}), 404
+    
+    del cart[product_id] 
+    return json.dumps({"success": True, "data": item}),200    
+    
+@app.route("/cart/")
+def get_cart():
+    data = list(cart.values())
+    if cart_item_count == 0:
+        return json.dumps({"data": "cart is empty!"}),200
+    return json.dumps({"success": True, "data": data}),200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
