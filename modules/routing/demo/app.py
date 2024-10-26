2644878840b8150fccfd13b85a089dad32e873d7
py1u.dev <py1u.dev@gmail.com>
2024-10-25 21:38:34 -0700

    create: task dictionary

--- modules/routing/demo/app.py
+++ modules/routing/demo/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
-import requests
-
+from flask import Flask, request
+import json
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+tasks = {
+    0: {
+        "id":0,
+        "description": "homework",
+        "done": False,
+    },
+    
+    1: {
+        "id": 1,
+        "description": "testing",
+        "done": False,
+    }
+}
+
+task_id_counter = 2
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
