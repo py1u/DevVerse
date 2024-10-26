cb96531bf513d7c23498f1a8bda9f71f8e2ff015
py1u.dev <py1u.dev@gmail.com>
2024-10-25 21:41:09 -0700

    create: create task endpoint

--- modules/routing/demo/app.py
+++ modules/routing/demo/app.py
@@ -32,6 +32,20 @@ def get_tasks():
         "data": data,
     }
     return json.dumps(response), 200
+    
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    global task_id_counter
+    body = json.loads(request.data)
+    description = body.get("description")
+    task = {
+        "id": task_id_counter,
+        "description": description,
+        "done": False
+    }
+    tasks[task_id_counter] = task
+    task_id_counter += 1
+    return json.dumps({"success": True, "data": task}), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
