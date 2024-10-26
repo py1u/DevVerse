5f88a34457b2181ae214ab3bf06539328118d6a5
py1u.dev <py1u.dev@gmail.com>
2024-10-25 21:39:43 -0700

    update: add get task endpoint

--- modules/routing/demo/app.py
+++ modules/routing/demo/app.py
@@ -23,5 +23,15 @@ tasks = {
 
 task_id_counter = 2
 
+
+@app.route("/tasks/")
+def get_tasks():
+    data = list(tasks.values())
+    response = {
+        "success": True,
+        "data": data,
+    }
+    return json.dumps(response), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
