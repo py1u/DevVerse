002a5509405b0612899ab9a90093163549cf630b
py1u.dev <py1u.dev@gmail.com>
2024-10-27 11:13:11 -0700

    update: create post req for tasks

--- modules/db/sqlite/app.py
+++ modules/db/sqlite/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
-
 app = Flask("__name__")
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/tasks/")
+def get_tasks():
+    pass
+
+@app.route("tasks/", methods=["POST"])
+def create_task():
+    pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
