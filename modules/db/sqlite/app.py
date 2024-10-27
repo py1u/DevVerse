43012f2e1126cb08f31af232ab4ec8c9c656076c
py1u.dev <py1u.dev@gmail.com>
2024-10-27 13:16:34 -0700

    feat: create task implementation with request import

--- modules/db/sqlite/app.py
+++ modules/db/sqlite/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 import json
 import db 
 
@@ -6,7 +6,7 @@ DB = db.DatabaseDriver()
 
 app = Flask("__name__")
 
-def sucess_res(data, code=200):
+def success_res(data, code=200):
     return json.dumps({"success": True, "data": data}), code
 
 def failure_res(message, code=404):
@@ -18,7 +18,14 @@ def get_tasks():
 
 @app.route("tasks/", methods=["POST"])
 def create_task():
-    pass
+    body = json.loads(request.data)
+    description = body.get("description")
+    task_id = DB.insert_task_table(description, False)
+    task = DB.get_task_by_id(task_id)
+    
+    if task is not None:
+        return success_res(task, 201)
+    return failure_res("something went wrong while creating task!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
