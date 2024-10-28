a0c7a8dede5f9f5b4e820789cd6e9382c6518b8b
py1u.dev <py1u.dev@gmail.com>
2024-10-28 16:41:34 -0700

    feat: db connection instance

--- modules/db/airbnb-admin/app.py
+++ modules/db/airbnb-admin/app.py
@@ -4,7 +4,12 @@ app = Flask("__main__")
 
 @app.route("/")
 def hello():
-    return "Uber"
+    return "Airbnb Admin"
+
+@app.route("/bookings/")
+def get_bookings():
+    pass
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ modules/db/airbnb-admin/db.py
@@ -0,0 +1,38 @@
+import os
+import sqlite3
+
+def singleton(cls):
+    instances = {}
+
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+class DatabaseDriver(object):
+
+    def __init__(self):
+        self.conn = sqlite3.connect(
+            "booking.db", check_same_thread = False
+        )
+        self.create_task_table()
+        
+    def create_task_table(self):
+        try:
+            self.conn.execute(
+            """
+                CREATE TABLE booking (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    address TEXT NOT NULL,
+                    rate INTEGER NOT NULL,
+                    open INTEGER DEFAULT 0
+                )
+            """
+            )
+            
+        except Exception as err:
+            print(err)
+            
+    
