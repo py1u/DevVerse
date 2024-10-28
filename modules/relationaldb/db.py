2a6b2d598c23302786c2d878538a35e5abc1b182
py1u.dev <py1u.dev@gmail.com>
2024-10-27 23:45:37 -0700

    refactor: using helper functions

--- modules/relationaldb/db.py
+++ modules/relationaldb/db.py
@@ -32,6 +32,7 @@ class DatabaseDriver(object):
         instance variable `conn`
         """
         self.conn = sqlite3.connect("task.db", check_same_thread=False)
+        self.conn.execute("PRAGMA foreign keys = 1")
         self.create_task_table()
         self.create_subtask_table()
         
@@ -51,73 +52,48 @@ class DatabaseDriver(object):
                 );
             """
             )
-        except Exception as e:
-            print(e)
+        except Exception as err:
+            print(err)
 
     def delete_task_table(self):
-        """
-        Using SQL, deletes a task table
-        """
         self.conn.execute("DROP TABLE IF EXISTS tasks;")
 
     def get_all_tasks(self):
-        """
-        Using SQL, gets all tasks in the task table
-        """
         cursor = self.conn.execute("SELECT * FROM tasks;")
-        tasks = []
-
-        for row in cursor:
-            tasks.append({"id": row[0], "description": row[1], "done": bool(row[2])})
+        tasks = parse_cursor(cursor, ["id", "description", "done"])        
         return tasks
 
     def insert_task_table(self, description, done):
-        """
-        Using SQL, adds a new task in the task table
-        """
         cursor = self.conn.execute("INSERT INTO tasks (description, done) VALUES (?, ?);", (description, done))
         self.conn.commit()
         return cursor.lastrowid
 
     def get_task_by_id(self, id):
-        """
-        Using SQL, gets a task by id
-        """
         cursor = self.conn.execute("SELECT * FROM tasks WHERE id = ?", (id,))
         for row in cursor:
-            return {"id": row[0], "description": row[1], "done": bool(row[2])}
+            return parse_row(row, ["id", "description", "done"])
         return None
 
     def update_task_by_id(self, id, description, done):
-        """
-        Using SQL, updates a task by id
-        """
         self.conn.execute(
             """
             UPDATE tasks
             SET description = ?, done = ?
             WHERE id = ?;
-        """,
+            """,
             (description, done, id),
         )
         self.conn.commit()
 
     def delete_task_by_id(self, id):
-        """
-        Using SQL, deletes a task by id
-        """
+        
         self.conn.execute(
             """
             DELETE FROM tasks
             WHERE id = ?;
-        """,
+            """,
             (id,),
         )
         self.conn.commit()
 
-#-- SUBTASKS --------------------------------------------------------
-
-
-# Only <=1 instance of the database driver
-# exists within the app at all times
 DatabaseDriver = singleton(DatabaseDriver)
