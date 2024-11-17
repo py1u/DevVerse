1391c1ce903ede6d3a6ca6ae30de44a40f033928
py1u.dev <py1u.dev@gmail.com>
2024-11-17 14:29:02 -0800

    feat: use pytest for read, update, delete apps

--- modules/testing/src/app.py
+++ modules/testing/src/app.py
@@ -3,42 +3,79 @@ import json
 
 app = Flask(__name__)
 
-jobs = {
-  "softwareEngineer": "Software Engineer",
-  "backendEngineer": "Backend Engineer",
-  "frontendEngineer": "Frontend Engineer",
-  "qaEngineer": "QA Engineer",
-  "softwareEngineerIntern": "Software Engineer Intern"
-}
-
-
-jobs_update = {
+job_db = {
   "softwareEngineer":{
    'job_id': 0,
    'title': "Software Engineer",
-   'desc': "SWE at Artr",
+   'description': "SWE at Artr",
    'available': True, 
    'date': "2024-10-8"
   },
   "backendEngineer":{
    'job_id': 1,
    'title': "Backend Engineer",
-   'desc': "Backend dev at Artr",
+   'description': "Backend dev at Artr",
    'available': False, 
    'date': "2024-10-2"
+  },
+  "frontendEngineer": {
+    'job_id': 2,
+    'title': "Frontend Engineer",
+    'description': "Frontend dev at Artr",
+    'available': False,
+    'date': "2024-10-9"
+  },
+  "qaEngineer": {
+    'job_id': 3,
+    'title': "QA Engineer",
+    'description': "QA dev at Artr",
+    'available': False,
+    'date': "2024-11-2"
+  },
+  "softwareEngineerIntern": {
+    'job_id': 4,
+    'title': "Software Engineer Intern",
+    'description': "Software engineer intern at Artr",
+    'available': False,
+    'date': "2024-11-8"
   }
 }
 
 applications_db = {}
-application_id = 0
+application_id = len(job_db)
 
 #added helper functions
 def client_success_res(data):
-  return json.dumps({"Success": True, "Data" : data}),200
+  return jsonify({"Success": True, "Data" : data}),200
 
 def client_failure_res(message):
-  return json.dumps({"Success": False, "Error": message}),404
+  return jsonify({"Success": False, "Error": message}),404
 
+def valid_form_apply(payload):
+  
+  name = payload.get("name")
+  email = payload.get("email")
+  phone = payload.get("phone")
+  education = payload.get("education")
+  major = payload.get("major")
+  
+  if not name:
+    return client_failure_res("Missing name field!")
+  
+  if not email:
+    return client_failure_res("Missing email field!")
+  
+  if not phone:
+    return client_failure_res("Missing phone field!")
+  
+  if not education:
+    return client_failure_res("Missing education field!")
+  
+  if not major:
+    return client_failure_res("Missing major field!")
+  
+  return payload
+  
 
 @app.route("/")
 def hello():
@@ -46,85 +83,84 @@ def hello():
 
 @app.route("/api/engineering/<string:role>/<int:job_id>", methods=["GET", "PUT", "DELETE"])
 def handle_job_description(role, job_id):
-  if role not in jobs_update:
+  if role not in job_db:
     return client_failure_res("Job not found!")
-    
+  
   if request.method == "GET":
-    return jsonify({"job_id": job_id,
-                       "title": jobs_update[role].get("title"), 
-                       "description": jobs_update[role].get("desc"), 
-                       "available": jobs_update[role].get("available"), 
-                       "date": jobs_update[role].get("date")
-                       }), 200
+    
+    position = job_db[role]
+    data = {"job_id": job_id,
+            "title": position.get("title"), 
+            "description": position.get("description"), 
+            "available": position.get("available"), 
+            "date": position.get("date")
+            }
+
+    return client_success_res(data)
   
   elif request.method == "PUT":
-    data = request.get_json()
-    if not data:
-      return json.dumps({"success": False, "error": "No data"}), 404
     
-    updated_data = {
-      "title": data.get("title", jobs[role]),
-      "description": data.get("description", "")
-    }
+    body = json.loads(request.data)
+    position = job_db.get(role)
+    if not position:
+      return client_failure_res("Job not found!")
     
-    return json.dumps({"success": True, "data": {"id": id, "role": role, "updated_data": updated_data}}), 200
+    position["job_id"] = position.get("job_id")
+    position["title"] = body.get("title")
+    position["description"] =  body.get("description")
+    position["available"] = body.get("available")
+    position["date"] = position.get("date")
+              
+    return client_success_res(position)
+  
   elif request.method == "DELETE":
-    job = jobs.get(role, None)
+    job = job_db[role]
     
     if not job:
-      return json.dumps({"success": False, "error": "Job not found!"}), 404
-    del jobs[role]
-    return json.dumps({"success": True, "data": job}), 200
+      return client_failure_res("Job not found!")
+    
+    del job_db[role]
+    return client_success_res(job)
   
 @app.route("/api/<string:role>/apply")
 def get_application(role):
-  return "Application"
+  if role not in job_db:
+    return client_failure_res("Job not found!")
+  
+  position = job_db[role]
+  print(f"retrieve {positon}")
+  
+  data = {
+    "job_id": position.get("job_id"),
+    "title": position.get("title"),
+    "desc": position.get("desc"),
+    "available": position.get("available"),
+    "date": position.get("date")
+  }
+  
+  return client_success_res(data)
 
 @app.route("/api/<string:role>/apply", methods=["POST"])
 def post_application(role):
   global application_id
-  data = request.json
   
-  name = data.get("name")
-  email = data.get("email")
-  phone = data.get("phone")
-  education = data.get("education")
-  major = data.get("major")
-  
-  if not name:
-    return json.dumps({"success": False, "error": "Missing name"}), 400
-  
-  if not email:
-    return json.dumps({"success": False, "error": "Missing email"}), 400
-  
-  if not phone:
-    return json.dumps({"success": False, "error": "Missing phone"}), 400
-  
-  if not education:
-    return json.dumps({"success": False, "error": "Missing education"}), 400
-  
-  if not major:
-    return json.dumps({"success": False, "error": "Missing major"}), 400
+  body = json.loads(request.data)
+  return_payload = validate_form_apply(body)
   
   applications_db[application_id] = {
     "application_id": application_id,
-    "role": role,
-    "name": name,
-    "email": email,
-    "phone": phone,
-    "education": education,
-    "major": major
+    "role": return_payload[role],
+    "name": return_payload[name],
+    "email": return_payload[email],
+    "phone": return_payload[phone],
+    "education": return_payload[education],
+    "major": return_payload[major]
   }
   
-  response = {
-    "message": "Application received!",
-    "role": role,
-    "applicant": applications_db[application_id]
-  }
-  
+  data = applications_db[application_id]
   application_id += 1
-  
-  return json.dumps({"success": True, "data": response}), 200
+
+  return client_success_res(data)
 
 @app.route("/api/<string:role>/apply/<int:application_id>", methods=["PUT"])
 def update_application(role, application_id):
@@ -133,7 +169,7 @@ def update_application(role, application_id):
   application = applications_db.get(application_id)
   
   if not application:
-    return json.dumps({"success": False, "error": "Application not found!"}), 400
+    return client_failure_res("Application not found!")
   
   name = data.get("name")
   email = data.get("email")
@@ -142,7 +178,7 @@ def update_application(role, application_id):
   major = data.get("major")
   
   if not any([name, email, phone, education, major]):
-    return json.dumps({"success": False, "error": "No field provided for update!"}), 400
+    return client_failure_res("No field provided for update!")
   
   if name: 
     application["name"] = name
@@ -168,20 +204,21 @@ def update_application(role, application_id):
     "applicant": application
   }
   
-  return json.dumps({"success": True, "data": response}), 200
+  return client_success_res(response)
 
 @app.route("/api/<string:role>/success/<int:application_id>")
 def confirm(role, application_id):
   application = applications_db.get(application_id)
   
   if not application:
-    return json.dumps({"success": False, "error": "Application not found!"}), 404
+    return client_failure_res("Application not found!")
   
   response = {
     "message": f"Application for {role} has been successfully submitted!",
   }
   
-  return json.dumps({"success": True, "data": response}), 200
+  return client_success_res(response)
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8000, debug=True)
+    
--- modules/testing/tests/test_app.py
+++ modules/testing/tests/test_app.py
@@ -21,8 +21,23 @@ def client():
     
 # test all possible jobs and their respective ids
 @pytest.mark.parametrize("role,job_id,expected_response_data", 
-                         [("softwareEngineer",0,{"job_id": 0, "title":"Software Engineer"}), 
-                          ("backendEngineer",1,{"job_id": 1, "title":"Backend Engineer"})
+                         [("softwareEngineer",0,
+                           {"Success": True, 
+                            "Data" : {"job_id": 0, 
+                                      "title":"Software Engineer",
+                                      "description": "SWE at Artr",
+                                      "available": True,
+                                      "date": "2024-10-8"
+                                      }
+                            }), 
+                          ("backendEngineer",1,
+                           {"Success": True, 
+                            "Data" : {"job_id": 1,
+                                      "title":"Backend Engineer",
+                                      "description": "Backend dev at Artr",
+                                      "available": False,
+                                      "date": "2024-10-2"}
+                            })
                           ])
 
 def test_get_any_role(client,role,job_id,expected_response_data):
@@ -34,6 +49,45 @@ def test_get_any_role(client,role,job_id,expected_response_data):
     
     assert data == expected_response_data
     
-   
-    
+
+# update a existing job title, description, and availability 
+@pytest.mark.parametrize("role, job_id, update_data, expected_update_data", 
+                         [("softwareEngineer", 1, 
+                           {"job_id": 1, 
+                            "title": "Software Engineer, Core Infra",
+                            "description": "core engineer",
+                            "available": False,
+                            "date": "2024-10-8"
+                            },  
+                           
+                           {"Success": True, 
+                            "Data":{"job_id": 1,
+                                    "title": "Software Engineer, Core Infra", 
+                                    "description": "core engineer", 
+                                    "available": False, 
+                                    "date": "2024-10-8"
+                                    }
+                                }
+                           ), ])
+def test_update_job(client, role, job_id, update_data, expected_update_data):
+  response = client.put(f"/api/engineering/{role}/{job_id}", json=update_data)
+  assert response.status_code == 200
+  
+
+  # send a GET request to check if the data was updated    
+  response = client.get(f"/api/engineering/{role}/{job_id}")
+  assert response.status_code == 200
+  
+  updated_data = response.get_json()
+  assert updated_data == expected_update_data
+  
+  
+# send a DELETE request to delete by role
+@pytest.mark.parametrize("role, job_id", [("softwareEngineer",1),])
+def test_delete_job(client, role, job_id):
+  deleted_data = client.delete(f"/api/engineering/{role}/{job_id}")  
+  response = client.get(f"/api/engineering/{role}/{job_id}")
+  assert response.status_code == 404
+  
+  
     
